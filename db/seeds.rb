33a74d49ca3a618c1b00fe1b8c303c136bbcf29f
nitesh <njha1987@gmail.com>
2016-11-15 15:43:57 +0530

    db deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "password",
-             password_confirmation: "password",
+User.create!(name:  "Nitesh Jha",
+             email: "njha1987@gmail.com",
+             password:              "Nikky@15",
+             password_confirmation: "Nikky@15",
              admin: 					true)
 
 Category.create!(name: "Apparel & Fashion")
@@ -22,19 +22,3 @@ Category.create!(name: "Textile & Fabrics")
 Category.create!(name: "Minerals & Metals")
 Category.create!(name: "Industrial Supplies")
 Category.create!(name: "Health & Beauty")
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content, category_id: 2, buysell: "Buy", title: "The title will come over here",
-   contact_detail: "njha1987@gmail.com" ) }
-end

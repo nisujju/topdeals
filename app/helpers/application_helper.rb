ef8a034203713fd18a7f14bef6ce1c8c7c951b98
nitesh <njha1987@gmail.com>
2016-11-20 14:16:10 +0530

    seo changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Best sales and business leads"
+    base_title = "Free sales & trade leads | Buy sell B2B free"
     if page_title.empty?
       base_title
     else

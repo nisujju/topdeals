fb4fff954698f084357442a623657ba55bbec035
nitesh <njha1987@gmail.com>
2016-12-03 11:36:33 +0530

    title change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "B2B sales leads, B2B trade leads & B2B marketing leads"
+    base_title = "B2B trade leads India, Buying leads, B2B Sales & Marketing"
     if page_title.empty?
       base_title
     else
